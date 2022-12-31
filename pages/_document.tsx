7da1b6ebcc6d6b36a92bbb15e53069e4057c28d4
mmnkuh <vladislavherasymenko@gmail.com>
2023-01-01 00:37:57 +0200

    chore: initial

---
+++ pages/_document.tsx
@@ -0,0 +1,20 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang='en' className='scroll-smooth'>
+        <Head>
+          <meta name='msapplication-TileColor' content='#000000' />
+          <meta name='theme-color' media='(prefers-color-scheme: light)' content='#fff' />
+          <meta name='theme-color' media='(prefers-color-scheme: dark)' content='#000' />
+        </Head>
+        <body className='bg-white text-black antialiased dark:bg-gray-900 dark:text-white'>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
